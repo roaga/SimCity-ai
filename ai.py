8cc828057c0432f000792db2637d9e217322e240
svedaant <38104633+svedaant@users.noreply.github.com>
2020-09-29 19:41:16 -0400

    Created ai.py. This is just notes for now.

---
+++ ai.py
@@ -0,0 +1,5 @@
+from game import takeTurn
+
+#state will be 10 x 10 x 8 array - 10 x 10 for board dimensions and 8 boards. We might need to add to this last dimension later
+#action space: all possible actions on a given turn
+#reward: happiness - population dependent
