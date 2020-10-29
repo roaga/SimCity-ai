a3655df5361f30b18a971fd6aa3bc2ee11e7628c
svedaant <38104633+svedaant@users.noreply.github.com>
2020-10-29 19:57:06 -0400

    Neural network added

--- ai.py
+++ ai.py
@@ -1,45 +1,44 @@
 import game 
-#import tensorflow as tf
+import tensorflow as tf
 import numpy as np
 from random import randint
-
+import torch
+from torch import nn
 #state will be 10 x 10 x 8 array - 10 x 10 for board dimensions and 8 boards. We might need to add to this last dimension later
 #action space: all possible actions on a given turn
 #reward: happiness - population dependent
 #Number of possible actions - can place 11 buildings anywhere or destroy on 10 x 10 grid (12 x 10 x 10) or wait (1)
-
-Q = np.zeros((10 * 10 * 8, 10 * 10 * 12 + 1))
-lr = .8
-y = .95
-num_episodes = 2000
-rList = []
-for i in range(num_episodes):
-    game.reset()
-    s = int(game.getIndex())
-    j = 0
-    while j < 99:
-        j+=1
-        #Choose an action by greedily (with noise) picking from Q table
-        a = int(np.argmax(Q[s,:]))
-        a += randint(1,10 * 10 * 12 + 1)*(1./(i+1))
-        a %= 10 * 10 * 12 + 1
-        a = int(a)
-        #Get new state and reward from environment
-        #print(1)
-        s1,r = game.takeAction(a)
-        print(r)
-        #Update Q-Table with new knowledge
-        Q[s,a] = Q[s,a] + lr*(r + y*np.max(Q[s1,:]) - Q[s,a])
-        #rAll += r
-        s = s1
-    #jList.append(j)
-    rList.append(rAll)
-
-j = 0
-game.rest()
-while j < 99:
-    a = a = np.argmax(Q[s,:] + np.random.randn(1,10 * 10 * 12 + 1)*(1./(i+1)))
-    game.takeAction(a)
-    print(game.building_map)
-    print(game.population)
+class NeuralNet(nn.Module):
+    def __init__(self):
+        super(NeuralNet, self).__init__()
+        self.conv = nn.Conv2d(128, 128, 1, 1)
     
+
+    def forward(self, x):
+        res = ResBlock()
+        for i in range(19):
+            x = res.forward(x)
+        x = torch.flatten(x) 
+        move = nn.Linear(x, 12*10*10+1)
+        move = torch.sigmoid(x) #move to be taken
+        val = nn.Linear(x, 1) #current state of the board as a number between -1 and 1
+        val = torch.tanh(x)
+        return x, val
+
+
+class ResBlock(nn.Module):
+    def __init__(self, inplanes=128, planes=128, stride=1, downsample=None):
+        super(ResBlock, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+    def forward(self, x):
+        residual = x
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += residual
+        out = F.relu(out)
