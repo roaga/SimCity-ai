865c93926e6999d9e37444779be6adad1407e7fe
svedaant <38104633+svedaant@users.noreply.github.com>
2020-11-02 20:30:13 -0500

    Debugged neural network

--- ai.py
+++ ai.py
@@ -1,9 +1,8 @@
 import game 
-import tensorflow as tf
 import numpy as np
-from random import randint
 import torch
 from torch import nn
+import torch.nn.functional as F
 #state will be 10 x 10 x 8 array - 10 x 10 for board dimensions and 8 boards. We might need to add to this last dimension later
 #action space: all possible actions on a given turn
 #reward: happiness - population dependent
@@ -11,30 +10,31 @@ from torch import nn
 class NeuralNet(nn.Module):
     def __init__(self):
         super(NeuralNet, self).__init__()
-        #self.conv = nn.Conv2d(128, 128, 1, 1) - maybe add this to the final 2 layers to improve efficiency
-    
 
     def forward(self, x):
+        x = x.astype(np.float32)
+        x = torch.from_numpy(x)
+        x = x.unsqueeze(0).unsqueeze(0)
         res = ResBlock()
         for i in range(19):
             x = res.forward(x)
         x = torch.flatten(x) 
-        move = nn.Linear(x, 12*10*10+1)
-        move = torch.sigmoid(x) #move to be taken
-        val = nn.Linear(x, 1) #current state of the board as a number between -1 and 1
-        val = torch.tanh(x)
-        return x, val
+        move = nn.Linear(800, 12*10*10+1)(x)
+        move = torch.sigmoid(move) #move to be taken
+        val = nn.Linear(800, 1)(x)
+        val = torch.tanh(val)
+        return move, val
 
 
 class ResBlock(nn.Module):
-    def __init__(self, inplanes=128, planes=128, stride=1, downsample=None):
+    def __init__(self, inplanes=1, planes=1, stride=1, downsample=None):
         super(ResBlock, self).__init__()
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=3, stride=stride,
+        self.conv1 = nn.Conv3d(inplanes, planes, kernel_size=3, stride=stride,
                      padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+        self.bn1 = nn.BatchNorm3d(planes)
+        self.conv2 = nn.Conv3d(planes, planes, kernel_size=3, stride=stride,
                      padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
+        self.bn2 = nn.BatchNorm3d(planes)
 
     def forward(self, x):
         residual = x
@@ -43,3 +43,8 @@ class ResBlock(nn.Module):
         out += residual
         out = F.relu(out)
         return out
+
+
+game.reset()
+model = NeuralNet()
+print(model.forward(game.getState())[0].size())
